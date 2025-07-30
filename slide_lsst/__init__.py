fc69713f2022264a84d912c5d48262fc66378af4
yizedong <yize.dong@outlook.com>
2025-07-30 15:02:47 -0400

    Update __init__.py

--- slide_lsst/__init__.py
+++ slide_lsst/__init__.py
@@ -1,5 +1,5 @@
 """
-LSST DECam Image Subtraction Package
+SLIDE: Subtracting LSST Images with DECam Exposures
 
 This package provides tools for performing image subtraction on LSST images using DECam templates,
 including PSF modeling, reference image downloading, and image subtraction.
@@ -18,9 +18,10 @@ from .image_processing import (
 )
 from .reference_download import (
     download_des_reference,
-    gaia3cat
+    download_decals_reference,
+    gaia3cat,
 )
-from .subtraction import perform_image_subtraction
+from .subtraction import perform_image_subtraction, lsst_decam_data_load, load_usesr_decam
 
 __all__ = [
     'gaia3cat',
@@ -34,5 +35,7 @@ __all__ = [
     'download_decals_reference',
     'lsst_visit_to_ccddata',
     'lsst_visit_to_psf',
-    'perform_image_subtraction'
+    'perform_image_subtraction',
+    'lsst_decam_data_load',
+    'load_usesr_decam'
 ] 
